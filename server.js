3ec140c87f7a9cf05b70e953ddb02d9dd905a791
Michael Zick <michaelzick@gmail.com>
2013-07-14 13:53:28 -0700

    removed example code; moved code into mongo auth

--- server.js
+++ server.js
@@ -20,98 +20,51 @@ http.createServer(app).listen(port, ipaddr, function(){
 
 // MongoDB
 app.use(express.bodyParser());
+var MongoClient = require('mongodb').MongoClient;
 
-var mongodb = require('mongodb');
-
-var App = function(){
-
-  // Scope
-
-  var self = this;
-
-  // Setup
-  
-  self.dbServer = new mongodb.Server(process.env.OPENSHIFT_MONGODB_DB_HOST, parseInt(process.env.OPENSHIFT_MONGODB_DB_PORT));
-  self.db = new mongodb.Db(process.env.OPENSHIFT_APP_NAME, self.dbServer, {auto_reconnect: true});
-  self.dbUser = process.env.OPENSHIFT_MONGODB_DB_USERNAME;
-  self.dbPass = process.env.OPENSHIFT_MONGODB_DB_PASSWORD;
-
-  self.ipaddr  = process.env.OPENSHIFT_INTERNAL_IP;
-  self.port    = parseInt(process.env.OPENSHIFT_INTERNAL_PORT) || 8080;
-
-  if (typeof self.ipaddr === "undefined") {
-    console.warn('No OPENSHIFT_INTERNAL_IP environment variable');
-  };
-  
-  
-   
-
-  // Web app logic
-  self.routes = {};
-  self.routes['health'] = function(req, res){ res.send('1'); };
-
-  self.routes['root'] = function(req, res){
-    self.db.collection('surfers').find().toArray(function(err, names) {
-        res.header("Content-Type:","text/json");
-        res.end(JSON.stringify(names));
-    });
-  };
-
-    // Webapp urls
-  
-  self.app  = express.createServer();
-  self.app.get('/health', self.routes['health']);
-  self.app.get('/', self.routes['root']);
- 
-
-
-
-  // Logic to open a database connection. We are going to call this outside of app so it is available to all our functions inside.
-
-  self.connectDb = function(callback){
-    self.db.open(function(err, db){
+app.get('/nemo', function(req, res){
+  MongoClient.connect("mongodb://$OPENSHIFT_MONGODB_HOST:$OPENSHIFT_MONGODB_PORT", function(err, db) {
+    if(!err) {
+      console.log("We are connected");
+    }
+    db.authenticate(dbUser, dbPass, {authdb: "admin"},  function(err, res){
       if(err){ throw err };
-      self.db.authenticate(self.dbUser, self.dbPass, {authdb: "admin"},  function(err, res){
-        if(err){ throw err };
-        callback();
+      db.collection("surfers", function(err, collection) {
+        collection.find().toArray(function(err, result) {
+          var surfers = [];
+          if (err) {
+            throw err;
+          } else {
+            for (i=0; i<result.length; i++) {
+              surfers[i] = result[i];
+            }
+            // surfers.reverse();
+            res.render('index.html', {surfers: surfers});
+          }
+        });
       });
     });
-  };
-  
-  
-  //starting the nodejs server with express
+  });
+});
 
-  self.startServer = function(){
-    self.app.listen(self.port, self.ipaddr, function(){
-      console.log('%s: Node server started on %s:%d ...', Date(Date.now()), self.ipaddr, self.port);
-    });
-  }
-
-  // Destructors
-
-  self.terminator = function(sig) {
-    if (typeof sig === "string") {
-      console.log('%s: Received %s - terminating Node server ...', Date(Date.now()), sig);
-      process.exit(1);
-    };
-    console.log('%s: Node server stopped.', Date(Date.now()) );
-  };
-
-  process.on('exit', function() { self.terminator(); });
-
-  self.terminatorSetup = function(element, index, array) {
-    process.on(element, function() { self.terminator(element); });
-  };
-
-  ['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT', 'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGPIPE', 'SIGTERM'].forEach(self.terminatorSetup);
-
-};
-
-//make a new express app
-var app = new App();
-
-//call the connectDb function and pass in the start server command
-app.connectDb(app.startServer);
+// app.post('/', function (req, res) {
+//   MongoClient.connect("mongodb://$OPENSHIFT_MONGODB_HOST:$OPENSHIFT_MONGODB_PORT/", function(err, db) {
+//     db.authenticate(dbUser, dbPass, {authdb: "admin"},  function(err, res){
+//       if(err){ throw err };
+//     });
+//     db.collection("surfers", function(err, collection) {
+//       collection.insert(
+//             {
+//               number: parseInt(req.body.custNum),
+//               firstName: req.body.firstName,
+//               lastName: req.body.lastName
+//             }, function (err, results, fields) {
+//           if (err) throw err;
+//           else res.send('success');
+//       });
+//     });
+//   });
+// });
 // /MongoDB
 
 app.engine('html', require('ejs').renderFile);
